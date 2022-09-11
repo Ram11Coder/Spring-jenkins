d81a5cf88b9960aabf445285e7900d151b4f2a80
Ram11Coder <rambeliver11@gmail.com>
2022-09-11 12:20:45 +0530

    initial commit

---
+++ src/test/java/com/jenkins/SpringJenkinsApplicationTests.java
@@ -0,0 +1,16 @@
+package com.jenkins;
+
+import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringJenkinsApplicationTests {
+	public static Logger logger = LoggerFactory.getLogger(SpringJenkinsApplication.class);
+
+	@Test
+	void contextLoads() {
+	}
+
+}
